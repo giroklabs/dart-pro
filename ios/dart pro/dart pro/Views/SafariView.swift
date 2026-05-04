9d55729913e65a548264b0b228cd36cd489073ed
greego <greego86@naver.com>
2026-05-05 08:22:59 +0900

    Fix build errors and complete app branding

---
+++ ios/dart pro/dart pro/Views/SafariView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import SafariServices
+
+// SwiftUI에서 SFSafariViewController를 사용하기 위한 공용 래퍼
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+    }
+}
