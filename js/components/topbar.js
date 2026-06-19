3bc0791ee72ab0541917408c8912ef181260e779
greego <greego86@naver.com>
2026-06-19 21:11:30 +0900

    style: remove settings button from topbar

--- js/components/topbar.js
+++ js/components/topbar.js
@@ -5,11 +5,6 @@ function renderTopbar() {
       <span class="material-symbols-outlined">search</span>
       <input type="text" id="global-search" placeholder="기업명, 종목코드로 검색..." />
     </div>
-    <div class="topbar-actions">
-      <button class="btn-secondary" onclick="location.hash='#/settings'" title="설정">
-        <span class="material-symbols-outlined" style="font-size:20px;vertical-align:middle;">settings</span>
-      </button>
-    </div>
   `;
 }
 
